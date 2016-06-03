9bea79ae5381d4141fd0d399fac65a95bb8ae84d
gfhg0 <grzkwasniewski@gmail.com>
2016-06-04 01:56:50 +0200

    Styling show page

--- app/models/music.rb
+++ app/models/music.rb
@@ -1,6 +1,6 @@
 class Music < ActiveRecord::Base
 	belongs_to :user
 	
-	has_attached_file :image, styles: { medium: "100x100#" }
+	has_attached_file :image, styles: { medium: "100x100#", large: "380x380>" }
   	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
